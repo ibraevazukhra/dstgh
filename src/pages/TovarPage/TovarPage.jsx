ed75b7d2cde4c5b403890a9e1242d72df26da124
Ибраева Зухра Ильсуровна <300704izi@gmail.com>
2024-03-20 08:50:01 +0300

    first

---
+++ src/pages/TovarPage/TovarPage.jsx
@@ -0,0 +1,17 @@
+export default function TovarPage (){
+    return(
+        <>
+            <div className="cardPage">
+                <div className="container">
+                    <div className="cardPage-content">
+                        <div className="right-card">
+                            <p className="name">Наименование</p>
+                            <p className="name">1000 руб</p><br />
+                            <p className="opisanie-txt">товар товра торва товраот  товаовтоат </p><br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
